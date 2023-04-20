a2d2b53cb5f7e84b81c39350f4878be782e2b380
sebastianlloves <sebastianlloves@gmail.com>
2023-04-20 16:58:08 -0300

    .

---
+++ Modulo 2/Ejercicios Alternativos/EjerciciosAlternativos.js
@@ -0,0 +1,9 @@
+//Modificar el estilo css según el tamaño de pantalla. La pantalla debe cargar con el estilo correspondiente (eventos load y resize)
+
+//Hacer una galería de imágenes
+
+//Función que maneja el recorrido de la galeria mediante las flechas
+
+//Escuchas de Evento
+
+//Acceso a Elementos Dinámicos mediante Delegación de Eventos
