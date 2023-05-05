be9c2dfe7b4d6cea8e710872da795fae522ad06c
sebastianlloves <sebastianlloves@gmail.com>
2023-05-05 15:49:44 -0300

    a

--- Modulo 3/Ejercicios Alternativos/Clase 4/Ejercicios.js
+++ Modulo 3/Ejercicios Alternativos/Clase 4/Ejercicios.js
@@ -21,3 +21,33 @@ function cambiarDiv(color, tiempo) {
         div.style.backgroundColor = color
     }, tiempo)
 }
+
+
+const h3 = document.createElement("h3")
+document.querySelector("form").insertAdjacentElement("afterend",h3)
+
+document.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const xhr = new XMLHttpRequest()
+    xhr.open('get','../../../Material Alternativo/Nueva carpeta/Material - Clase 04/bd.json')
+    xhr.send()
+
+    xhr.addEventListener("load", () => {
+        
+        let usuario_valido = false
+        if( xhr.status == 200){
+            const respuesta = JSON.parse(xhr.response)
+
+            const entrada_usuario = document.querySelector("#usuario").value
+            const entrada_password = document.querySelector("#password").value
+
+            for (const objeto of respuesta) {
+                if( entrada_usuario == objeto.user && entrada_password == objeto.pass ) usuario_valido = true
+            }
+        }
+
+        usuario_valido? h3.innerText = 'Es un usuario válido' :  h3.innerText = 'NO es un usuario válido' 
+    })
+})
+
