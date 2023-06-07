fef12dad386ae9902a508e69a041e1833cbba21a
sebastianlloves <sebastianlloves@gmail.com>
2023-06-07 00:06:14 -0300

    .

--- Modulo 6/Desafios/desafio1.js
+++ Modulo 6/Desafios/desafio1.js
@@ -1,18 +1,35 @@
-const inputs = document.querySelectorAll("input")
+/* const inputs = document.querySelectorAll("input")
 
 let states = {}
 
-
-inputs.forEach ( input => {
-    states[`input_${input.id}`] = false
-})
-
 const Required = (input) => {
-    // states[input.id] 
     document.addEventListener("focusout", e => {
-        if(e.target == input && input.value == '') states[`input_${input.id}`] = false
+        if(e.target === input && input.value === '') states[`input_${input.id}`] = false
         else states[`input_${input.id}`] = true
         console.log(states)
     })
 }
 
+const reportValidations = () => {
+
+}
+
+inputs.forEach ( input => {
+    states[`input_${input.id}`] = false
+})
+
+console.log(states); */
+/* function funcion (){
+    return {nombre:"Seba"}
+}
+
+funcion().nombre = 'Pepe'
+
+console.log(funcion().nombre); */
+
+const obj = {nombre: 'Seba'}
+const obj1 = obj
+
+obj.nombre = 'Pepe'
+console.log(obj);
+console.log(obj1);
