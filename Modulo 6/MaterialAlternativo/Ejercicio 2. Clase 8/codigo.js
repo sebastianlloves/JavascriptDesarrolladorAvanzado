6a87e603dc46a5aa4901b32102ff8cc3f7543363
sebastianlloves <sebastianlloves@gmail.com>
2023-06-09 01:18:03 -0300

    ,

---
+++ Modulo 6/MaterialAlternativo/Ejercicio 2. Clase 8/codigo.js
@@ -0,0 +1,39 @@
+
+
+const visor = document.getElementById("result")
+visor.innerText = 0
+
+
+const numeros = document.querySelectorAll(".number")
+
+numeros.forEach( boton => console.log(Number(boton.innerHTML)) )
+
+function stateCalculadora () {
+    return {
+        state: {
+            resultado: 0,
+            entrada: [],
+            operador : ''
+        },
+        subscribers : [],
+        suscribe: function (subscriber){
+            this.subscribers.push(subscriber)
+        }, 
+        notify: function (value) {
+            this.subscribers.forEach( subscriber => {
+                subscriber.notify(value)
+            } )
+        },
+        setState
+    }
+}
+
+function setState (newState) {
+    const state = this.state
+    for (const key in newState) {
+        if (state.hasOwnProperty(key)) {
+            state[key] = newState[key];
+        }
+    }
+}
+
