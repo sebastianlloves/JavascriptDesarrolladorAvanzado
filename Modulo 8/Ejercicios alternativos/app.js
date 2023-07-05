26433b555c3dce3b1ed13a2ecda7e89c0c9e5be5
sebastianlloves <sebastianlloves@gmail.com>
2023-07-05 01:20:31 -0300

    .

--- Modulo 8/Ejercicios alternativos/app.js
+++ Modulo 8/Ejercicios alternativos/app.js
@@ -1,7 +1,10 @@
 import { hacerFetch } from "./utils/fetchUser.js";
 import { comportamientoElementos } from "./utils/displayUser.js";
+import { mostrarData } from "./utils/mostrarData.js";
 
 (async function () {
     const datos_utiles = await hacerFetch("https://randomuser.me/api/")
     comportamientoElementos(datos_utiles);
+    const botones = document.querySelectorAll(".icon")
+    mostrarData(botones[4], datos_utiles)
 })()
