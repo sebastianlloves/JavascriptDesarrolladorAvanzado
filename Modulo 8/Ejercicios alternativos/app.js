5887db057d5e14edeb1ece9463730d55428aa68b
sebastianlloves <sebastianlloves@gmail.com>
2023-07-05 01:10:30 -0300

    .

--- Modulo 8/Ejercicios alternativos/app.js
+++ Modulo 8/Ejercicios alternativos/app.js
@@ -1,8 +1,7 @@
 import { hacerFetch } from "./utils/fetchUser.js";
+import { comportamientoElementos } from "./utils/displayUser.js";
 
 (async function () {
     const datos_utiles = await hacerFetch("https://randomuser.me/api/")
-    console.log(datos_utiles);
-    /* let {name: obj_nombre, email, dob: {age, date: cumple}, location: {street: {number: numero, name: calle}}, phone, login: {password: contrasena}} = random_user.results[0]
-    console.log(obj_nombre, email, cumple, `${numero}, ${calle}`, phone, contrasena); */
+    comportamientoElementos(datos_utiles);
 })()
--- Modulo 8/Ejercicios alternativos/utils/fetchUser.js
+++ Modulo 8/Ejercicios alternativos/utils/fetchUser.js
@@ -2,9 +2,9 @@ export async function hacerFetch (url){
     const res = await fetch(url)
     if(!res.ok) throw new Error ("No conectó")
     const random_user = await res.json()
-    let {name: obj_nombre, email: correo, dob: {age, date}, location: {street: {number: numero, name: calle}}, phone: telefono, login: {password: contrasena}} = random_user.results[0]
-    const direccion = `${numero}, ${calle}.`,
-        nombre = `${obj_nombre.title} ${obj_nombre.first} ${obj_nombre.last}`,
-        cumple = `${new Date(date).toLocaleDateString()}`
-    return {nombre, correo, cumple, direccion, telefono, contrasena}
+    let {name: obj_nombre, email, dob: {age, date}, location: {street: {number: numero, name: calle}}, phone, login: {password}, picture:{large: src_imagen}} = random_user.results[0]
+    const street = `${numero}, ${calle}.`,
+        name = `${obj_nombre.title} ${obj_nombre.first} ${obj_nombre.last}`,
+        birthday = `${new Date(date).toLocaleDateString()}`
+    return {name, email, birthday, street, phone, password, src_imagen}
 }
