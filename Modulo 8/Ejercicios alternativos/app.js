eef6fb724c5fa47fc442b4f61f310796a4c48cf1
sebastianlloves <sebastianlloves@gmail.com>
2023-07-06 15:57:32 -0300

    .

--- Modulo 8/Ejercicios alternativos/app.js
+++ Modulo 8/Ejercicios alternativos/app.js
@@ -5,20 +5,27 @@ import { mostrarData } from "./utils/mostrarData.js";
 const btn = document.querySelector(".btn")
 let datos_utiles
 
-async function showUser(e = {target: btn}) {
+async function showUser(e = {target: btn}, aprovechar_local) {
     if(e.target.matches(".btn")){
-        datos_utiles = await hacerFetch("https://randomuser.me/api/")
+        // localStorage.clear()
+        if(Object.keys(localStorage).includes('data') && aprovechar_local) datos_utiles = JSON.parse(localStorage.getItem("data")) 
+        else {
+            datos_utiles = await hacerFetch("https://randomuser.me/api/")
+            localStorage.setItem("data", JSON.stringify(datos_utiles))
+        }
+        
         comportamientoElementos(datos_utiles);
         document.addEventListener("mouseover", e =>{
             if(e.target.matches(".icon")) mostrarData(e.target, datos_utiles)        
         })
+        console.log(localStorage)
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    showUser()
+    showUser( undefined, true)
 })
 
 document.addEventListener("click", e => {
-    showUser(e)
+    showUser(e, false)
 })
