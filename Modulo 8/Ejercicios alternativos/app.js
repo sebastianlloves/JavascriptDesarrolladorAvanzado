6cebb1549ab0c10a5f257787b9443d94b8ad34c3
sebastianlloves <sebastianlloves@gmail.com>
2023-07-06 01:25:03 -0300

    .

--- Modulo 8/Ejercicios alternativos/app.js
+++ Modulo 8/Ejercicios alternativos/app.js
@@ -2,21 +2,23 @@ import { hacerFetch } from "./utils/fetchUser.js";
 import { comportamientoElementos } from "./utils/displayUser.js";
 import { mostrarData } from "./utils/mostrarData.js";
 
-async function showUser() {
-    let datos_utiles = await hacerFetch("https://randomuser.me/api/")
-    comportamientoElementos(datos_utiles);
-    document.addEventListener("mouseover", e =>{
-        if(e.target.matches(".icon")) mostrarData(e.target, datos_utiles)
-    })
-    /* document.addEventListener("click", async e => {
-        if(e.target.matches(".btn")){
-            e.preventDefault()
-            datos_utiles = await hacerFetch("https://randomuser.me/api/")
-            comportamientoElementos(datos_utiles);
-        }
-    }) */
-}
-showUser()
-
 const btn = document.querySelector(".btn")
-btn.addEventListener("click", showUser)
+let datos_utiles
+
+async function showUser(e = {target: btn}) {
+    if(e.target.matches(".btn")){
+        datos_utiles = await hacerFetch("https://randomuser.me/api/")
+        comportamientoElementos(datos_utiles);
+        document.addEventListener("mouseover", e =>{
+            if(e.target.matches(".icon")) mostrarData(e.target, datos_utiles)        
+        })
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showUser()
+})
+
+document.addEventListener("click", e => {
+    showUser(e)
+})
